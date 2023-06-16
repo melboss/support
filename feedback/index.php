6bd011dba73f3c570ceae4b0fd9862de305bff77
Melboss Music <developer@melboss.com>
2023-06-16 20:30:36 +0200

    Update index.php

--- feedback/index.php
+++ feedback/index.php
@@ -307,7 +307,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
           <div class="col-12 mb-4">
             <label for="email" class="form-label fs-base">Email</label>
             <?php if ($_GET["email"]) : ?> 
-              <input type="email" id="email"  name="email" class="form-control form-control-lg" required disabled value='<?php echo $_GET["email"]; ?>'>
+              <input type="email" id="email"  name="email" class="form-control form-control-lg" required value='<?php echo $_GET["email"]; ?>'>
               <?php  else : ?>
             <input type="email" id="email"  name="email" class="form-control form-control-lg" required>
             <?php endif; ?>
